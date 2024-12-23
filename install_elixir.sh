2184234abc85dace5fe278bdda1efdb27af51a9b
Haukur Rosinkranz <hauxir@gmail.com>
2024-12-23 15:46:45 +0000

    upgrade elixir to 1.18 and OTP to 27.2

--- install_elixir.sh
+++ install_elixir.sh
@@ -3,28 +3,27 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.0
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.bashrc
 source ~/.bashrc
 
-bash /install_erlang.sh
 # Install Erlang plugin
-#asdf plugin-add erlang
+asdf plugin-add erlang
 
 # Install a compatible version of Erlang for Elixir 1.15
-#asdf install erlang 27.0.1
+asdf install erlang 27.2
 
 # Set the installed Erlang version as the global version
-#asdf global erlang 27.0.1
+asdf global erlang 27.2
 
 # Install Elixir plugin
 asdf plugin-add elixir
 
 # Install Elixir 1.15
-asdf install elixir 1.17.2-otp-27
+asdf install elixir 1.18.0-otp-27
 
 # Set Elixir 1.15 as the global version
 
-asdf global elixir 1.17.2-otp-27
+asdf global elixir 1.18.0-otp-27
 
 ln -s /root/.asdf/shims/iex /usr/bin/iex
 ln -s /root/.asdf/shims/erl /usr/bin/erl
 ln -s /root/.asdf/shims/mix /usr/bin/mix
 
-echo "Erlang and Elixir 1.17 installation complete."
+echo "Erlang and Elixir 1.18 installation complete."
